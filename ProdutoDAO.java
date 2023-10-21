4ed6eba52a39d7dcf77bbcc057f87294098917a8
lucasnovaesc <81600282+lucasnovaesc@users.noreply.github.com>
2023-10-21 12:09:22 -0300

    teste: add arquivo aleatorio

---
+++ ProdutoDAO.java
@@ -0,0 +1,27 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Conexao.Conexao;
+
+public class ProdutoDAO {
+    
+    public void cadastrarProduto(Produto produto) {
+
+        String sql = "INSERT INTO PRODUTO (ID, NOME, PRECO, DESCRICAO) VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, produto.getId());
+            ps.setString(2, produto.getNome());
+            ps.setDouble(3, produto.getPreco());
+            ps.setString(4, produto.getDescricao());
+
+            ps.execute();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
